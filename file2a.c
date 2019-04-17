afc3b5e621bafd335a22a0cec702fbf7fae0cb5d
Jatin Uppal <42096994+Jatinuppal1@users.noreply.github.com>
2019-04-18 05:08:36 +0530

    Minute change

--- file2a.c
+++ file2a.c
@@ -2,7 +2,7 @@
 int main()
 {
 	printf("_____Introduction_____\n");
-	printf("My name is Jatin\n");
+	printf("My name is Jatin 500069156\n");
 	printf("I am learning SCM\n");
 	printf("This is a very cool subject to learn.");
 	return 0;
